779f153eda8b68ef6328919a4affe4ac7115106d
DheerajVerma <thedheeraj.verma@gmail.com>
2021-08-02 16:17:07 +0530

    inital commit

---
+++ SampleProjectCICD/SampleProjectCICD/SampleProjectCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectCICDApp.swift
+//  SampleProjectCICD
+//
+//  Created by Dheeraj Verma on 02/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
